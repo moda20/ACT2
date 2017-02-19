932c4414a6c909d4b719a7327fb53d7fc03a8ac9
jaxle <jaxlegrand@hotmail.fr>
2017-02-19 19:42:26 +0100

    First time

---
+++ www/js/index.js
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Created by jaxle on 26/01/2017.
+ */
+
+$(function () {
+    $( "#gotop2" ).on('click',function( event ) {
+        console.log("RRRRRR")
+        $.mobile.navigate( "#nd", {
+            info: "info about the #bar hash"
+        });
+        $( window ).on( "navigate", function( event, data ) {
+            console.log(data);
+            console.log( data.state.info );
+            console.log( data.state.direction );
+            console.log( data.state.url );
+            console.log( data.state.hash );
+        });
+        window.history.back();
+    });
+
+
+    $("#st").on('navigate', function (event, data) {
+        console.log(data.state.url);
+        console.log(data.state.hash);
+        console.log(data.state.direction);
+    });
+    $("#goback").on('click', function(event){
+        console.log("HERE IT IS BACK");
+        $.mobile.navigate("#st",{
+            info: "HASHCODE"
+        });
+        $( window ).on( "navigate", function( event, data ) {
+            console.log(data);
+            console.log( data.state.info );
+            console.log( data.state.direction );
+            console.log( data.state.url );
+            console.log( data.state.hash );
+        });
+        window.history.back();
+    })
+});
+
